2d4754f29ab15b54ddef5724b9a2363409302a34
JRSubrean <roldan_subrean_jr@outlook.com>
2016-12-22 18:12:50 -0600

    Login Window
    This is an app that shows a login page, where you can input your username and password in. Along with being written in C-Sharp, it also uses some SQL server as well. I designed this in Visual Studio 2015.

---
+++ LoginWindow/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\JRSubrean\Documents\LoginInfo.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From LoginInfo where Username ='" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", connect);
+            DataTable tableOfData = new DataTable();
+            sda.Fill(tableOfData);
+            if (tableOfData.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+
+                Main aquaPage = new LoginWindow.Main();
+                aquaPage.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username and/or Password combination. Please try again.");
+            }
+        }
+    }
+}
